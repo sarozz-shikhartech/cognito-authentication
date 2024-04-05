503c2c2db808816b1e74a291ea5bb2ea020841dc
Saroj Shrestha <saroj.s@shikhartech.com>
2024-04-05 14:20:16 +0545

    cognito service completed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\ResponseTrait;
+use App\Http\Services\CognitoService;
+use App\Http\Services\MainService;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    use ResponseTrait;
+    protected CognitoService $cognitoService;
+    protected MainService $mainService;
+
+    /**
+     * @param CognitoService $cognitoService
+     * @param MainService $mainService
+     */
+    public function __construct(CognitoService $cognitoService, MainService $mainService)
+    {
+        $this->cognitoService = $cognitoService;
+        $this->mainService = $mainService;
+    }
+
+
 }
